431228ff089622d4d7b3839cf51dd7bde343f03e
Mike Luz <onlymikeluz@gmail.com>
2017-05-29 14:17:47 -0400

    working on routes and getting data to client store

--- app/components/Dashboard.js
+++ app/components/Dashboard.js
@@ -1,15 +1,33 @@
 import React from 'react'
+import RaisedButton from 'material-ui/RaisedButton';
 
-const Dashboard = ({ user, children }) => (
+class Dashboard extends React.Component {
 
-  <div>
-   {user && <h1>Welcome back, {user.name}</h1>}
-  </div>
-)
+	constructor(props) {
+		super(props)
+
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick() {
+		this.props.getAllPubs();
+	}
+
+	render() {
+		return (
+		  <div>
+		   {this.props.user && <h1>Welcome back, {this.props.user.name}</h1>}
+		   <RaisedButton label="Publications" onClick={this.handleClick}/>
+
+		  </div>
+		)
+	}
+}
 
 import {connect} from 'react-redux'
+import {getAllPubs} from '../reducers/allPubs'
 
 export default connect(
   ({ auth }) => ({ user: auth }),
-  {},
+  {getAllPubs},
 )(Dashboard)
